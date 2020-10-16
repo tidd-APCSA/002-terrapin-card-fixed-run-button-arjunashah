ec1dc01053ac4a6d5eca7ea60d650641afbc28f3
Arjun Shah <arjunashah445@gmail.com>
2020-10-16 13:13:10 +0000

    This jawnt is making me mad now...

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,6 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        System.out.println("hey");
         
         TerrapinCard cardBilly= new TerrapinCard (20);
         TerrapinCard cardBrian = new TerrapinCard (30);
